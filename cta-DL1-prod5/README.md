c46f023fe95a80b15fb2ced719dab86134c3de13
Gernot Maier <maierg@Gernots-MBP.fritz.box>
2020-11-12 08:40:34 +0100

    production version for prod5; added prod5b North

--- cta-DL1-prod5/README.md
+++ cta-DL1-prod5/README.md
@@ -5,7 +5,7 @@
 Building expects that a tar ball of hessioxx (hessioxxx.tar.gz) is available in the building directory.
 
 ```
-$ docker build -t eventdisplay-cta-dl1 .
+$ docker build -t eventdisplay-cta-dl1-prod5 .
 ```
 
 Download hessioxx from https://www.mpi-hd.mpg.de/hfm/CTA/MC/Software/Testing/hessioxxx.tar.gz (passwd applies)
@@ -17,7 +17,7 @@ Sim_telarray files and output evndisp root files are read and writting from the 
 To analysis a prod5 sim_telarray file:
 
 ```
-$  docker run --rm -it -v "$(pwd)/data:/data" eventdisplay-cta-dl1 \
+$  docker run --rm -it -v "$(pwd)/data:/data" eventdisplay-cta-dl1-prod5 \
      /evndisp/run.sh \
      /data/gamma_20deg_0deg_run9___cta-prod5-paranal_desert-2147m-Paranal-dark_cone10.simtel.zst \
      CTA.prod5S.BL-4LSTs25MSTs70SSTs-MSTF.lis
@@ -26,7 +26,7 @@ $  docker run --rm -it -v "$(pwd)/data:/data" eventdisplay-cta-dl1 \
 To run the container in bash and analyse a prod5 sim_telarray file:
 
 ```
-$ docker run --rm -it -v "$(pwd)/data:/data" eventdisplay-cta-dl1 bash
+$ docker run --rm -it -v "$(pwd)/data:/data" eventdisplay-cta-dl1-prod5 bash
 $ run.sh \
     /data/gamma_20deg_0deg_run9___cta-prod5-paranal_desert-2147m-Paranal-dark_cone10.simtel.zst \
     CTA.prod5S.BL-4LSTs25MSTs70SSTs-MSTF.lis
--- cta-DL1-prod5/run.sh
+++ cta-DL1-prod5/run.sh
@@ -3,21 +3,34 @@
 # run a Eventdisplay DL1 analysis for CTA prod5 simulations
 #
 
-if [ $# -lt 2 ]; then
+if [ $# -lt 1 ]; then
 	echo "
-	./run.sh <sim_telarray file> <layout file>
+	./run.sh <sim_telarray file> [layout file]
 	echo
-	echo <layout file>   e.g., CTA.prod5S.BL-4LSTs25MSTs70SSTs-MSTF.lis
+	echo [layout file (optional)]   e.g., CTA.prod5S.BL-4LSTs25MSTs70SSTs-MSTF.lis
 	"
         exit
 fi
 
 DATAFILE=${1}
 LAYOUTFILE=${2}
+# select automatically the corresponding hyperlayout
+if [[ -z ${LAYOUTFILE} ]]; then
+    if [[ $DATAFILE == *"paranal"* ]]; then
+        LAYOUTFILE="CTA.prod5S.hyperarray.lis"
+    else
+        if  [[ $DATAFILE == *"prod5b"* ]]; then
+            LAYOUTFILE="CTA.prod5bN.hyperarray.lis"
+        else
+            LAYOUTFILE="CTA.prod5N.hyperarray.lis"
+        fi
+    fi
+fi
+     
 OUTPUTFILE=$(basename ${DATAFILE} .zst)
 rm -f /tmp/${OUTPUTFILE}*
 
-# select correct calibration file
+# calibration file
 if [[ $DATAFILE == *"dark"* ]]; then
 	IPRFILE=${OBS_EVNDISP_AUX_DIR}/Calibration/prod5/prod5-IPR.root
 else
@@ -43,12 +56,12 @@ fi
 # run analysis
 ${EVNDISPSYS}/bin/CTA.convert_hessio_to_VDST -c ${IPRFILE} \
 	-a $OBS_EVNDISP_AUX_DIR/DetectorGeometry/${LAYOUTFILE} \
-	-o /tmp/tmp.dst.root \
+	-o /tmp/${OUTPUTFILE}.dst.root \
 	${DATAFILE} > /tmp/${OUTPUTFILE}.convert.log
 
 $EVNDISPSYS/bin/evndisp -averagetzerofiducialradius=0.5 -imagesquared \
        	-reconstructionparameter EVNDISP.prod5.reconstruction.runparameter \
-       	-sourcefile /tmp/tmp.dst.root \
+       	-sourcefile /tmp/${OUTPUTFILE}.dst.root \
        	-outputfile /tmp/${OUTPUTFILE}.root > /tmp/${OUTPUTFILE}.evndisp.log
 
 ###########
@@ -57,9 +70,9 @@ if [ -e /tmp/${OUTPUTFILE}.root ]; then
 	# move log files into evndisp output file
 	if [ -e /tmp/${OUTPUTFILE}.convert.log ]; then
 		$EVNDISPSYS/bin/logFile convLog /tmp/${OUTPUTFILE}.root /tmp/${OUTPUTFILE}.convert.log
-        fi
+    fi
 	if [ -e /tmp/${OUTPUTFILE}.evndisp.log ]; then
 		$EVNDISPSYS/bin/logFile evndispLog /tmp/${OUTPUTFILE}.root /tmp/${OUTPUTFILE}.evndisp.log
-        fi
+    fi
 	mv -f -v /tmp/${OUTPUTFILE}.root /data/
 fi
